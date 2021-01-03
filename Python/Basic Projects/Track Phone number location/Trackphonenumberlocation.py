ee405a1b1bd57a8a4dcb47c072383eac169ea5c0
Kushal Das <daskushal980@gmail.com>
2021-01-03 11:35:12 +0530

    added source code

--- Python/Basic Projects/Track Phone number location/Trackphonenumberlocation.py
+++ Python/Basic Projects/Track Phone number location/Trackphonenumberlocation.py
@@ -1,6 +1,17 @@
-import phonenumbers
+import phonenumbers ,pyttsx3
 from phonenumbers import geocoder, carrier
-x=input('enter')
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+speak('Please enter the number along with country code' )
+x=input("Please enter the number along with country code ")
+
+
+
 # Parsing String to Phone number
 phoneNumber = phonenumbers.parse(x)
 
@@ -11,5 +22,9 @@ Carrier = carrier.name_for_number(phoneNumber, 'en')
 Region = geocoder.description_for_number(phoneNumber, 'en')
 
 # Printing the carrier and region of a phone number
+
 print(Carrier)
+speak('Your carrier name'+Carrier)
 print(Region)
+speak(Region+'is your region')
+speak('Thank You !')
