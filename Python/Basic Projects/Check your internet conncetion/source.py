2151e35182d7c4269757c6806327d302ebfa8098
Kushal Das <daskushal980@gmail.com>
2021-01-04 20:30:10 +0530

    Create source.py

---
+++ Python/Basic Projects/Check your internet conncetion/source.py
@@ -0,0 +1,16 @@
+import urllib
+from urllib.request import urlopen
+def connectionCheck():
+    try:
+
+
+        urlopen('https://www.google.com',timeout=1)
+        return True
+    except urllib.error.URLError as Error:
+        return False
+
+if connectionCheck():
+    print("Yes your internet is connected")
+else:
+    print("opps!😅 your internet is not connected with your System")
+© 2021 GitHub, Inc.
