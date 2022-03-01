9eb30029c42d1584c36e56a238e52725f5d5f51a
Sai Aditya Viswanadham <71250738+saiadityaviswanadham@users.noreply.github.com>
2022-03-01 18:06:24 +0530

    Updated Chat feature - Converse

---
+++ Web Development/Advanced/Converse--Client-Chat-Web-Application-main/README.md
@@ -0,0 +1,10 @@
+## Converse is a Client Chat Web Application build and deployed using Firebase
+
+### Services Used: 
+
+Firebase Authentication
+Cloud Firestore
+Firebase SDK for Cloud Storage
+Firebase Cloud Messaging
+Firebase Performance Monitoring
+Firebase Hosting
---
+++ Web Development/Advanced/Converse--Client-Chat-Web-Application-main/cloud-functions-angular/src/firebase-messaging-sw.js
@@ -0,0 +1,10 @@
+importScripts('https://www.gstatic.com/firebasejs/8.3.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.1/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  // TODO add your messagingSenderId
+  messagingSenderId: '662518903527'
+});
+var messaging = firebase.messaging();
