85701710444c0d1330a5d3bdcdf12c4820f02ec8
mayank-del <82238106+mayank-del@users.noreply.github.com>
2022-03-13 20:36:56 +0530

    Update scoreboard.py

--- Web Development/Basic/Ping-pong-Python/scoreboard.py
+++ Web Development/Basic/Ping-pong-Python/scoreboard.py
@@ -3,7 +3,7 @@ from turtle import Turtle
 
 class Scoreboard(Turtle):
 
-    def __init__(self):
+    def __init__(self):         
         super().__init__()
         self.color("white")
         self.penup()
@@ -12,17 +12,17 @@ class Scoreboard(Turtle):
         self.r_score = 0
         self.update_scoreboard()
 
-    def update_scoreboard(self):
+    def update_scoreboard(self):    # Function responsible for position of scoreboard's position, font ,styling.
         self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
         self.goto(100, 200)
         self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
 
-    def l_point(self):
+    def l_point(self):          #this function is responsible for increasing left player's score
         self.l_score += 1
         self.update_scoreboard()
 
-    def r_point(self):
+    def r_point(self):      #this function is responsible for increasing right player's score
         self.r_score += 1
         self.update_scoreboard()
