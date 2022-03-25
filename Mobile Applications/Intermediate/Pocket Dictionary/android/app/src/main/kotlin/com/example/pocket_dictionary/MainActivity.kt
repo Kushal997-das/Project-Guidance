b26798230148591c89a0bf1d10795fe354b0f13b
ifp2022 <97889481+ifp2022@users.noreply.github.com>
2022-03-25 16:40:38 +0530

    Pocket Dictionary App

---
+++ Mobile Applications/Intermediate/Pocket Dictionary/android/app/src/main/kotlin/com/example/pocket_dictionary/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pocket_dictionary
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Mobile Applications/Intermediate/Pocket Dictionary/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:pocket_dictionary/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        home: HomePage());
+  }
+}
