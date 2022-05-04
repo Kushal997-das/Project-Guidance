588d8f2bfd8cc688fd1bf7fde4e7600ac9b09f17
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-04 13:26:01 -0700

    :bug: not showing result fixed

--- Desktop Application/Basic/Python/Age calculator/age.py
+++ Desktop Application/Basic/Python/Age calculator/age.py
@@ -5,14 +5,14 @@ key3 = now.month
 d1 = datetime.date.today()
 num = 0
 try:
-    birthday = input("When is your birthday?:")
+    birthday = input("When is your birthday?")
     birthday = datetime.datetime.strptime(birthday,"%d/%m/%Y",).date()
     key2 = birthday.year+18
-    age = now.year - birthday.year12
+    age = now.year - birthday.year
     b_month = birthday.month
     n_year = now.year
     b_year = birthday.year
-
+######################################################################################################################################################################
     while b_year < n_year:
         year1 = 12                          # 1 year = 12 months
         num = num + 1
@@ -21,7 +21,7 @@ try:
     months = num * 12 + num2 - 1
     days = now.day - birthday.day
     # print(days)
-
+#######################################################################################################################################################################
     if birthday <= datetime.date.today() and key2 <= now.year:
         print("Your birthday is "+ birthday.strftime('%d, %B %Y'), "and Your Age is", age, "years and", num2-1,"months and", days, "days")
     elif age < 0:
@@ -30,4 +30,4 @@ try:
         print("You are under 18", "and Your Age is", age, "years and", num2-1,"months and", days, "days")
 except:
     print("date or format is invalid")
-
+25
