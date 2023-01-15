592fdfc49d0f73bb078a3addf4eb8a0e6575da03
unknown <goyalhim69@gmail.com>
2023-01-15 11:35:24 +0530

    add eComm website to project guidance in Web Development/Advanced/

---
+++ Web Development/Advanced/E-Comm Apparels Store Website/Readme.md
@@ -0,0 +1,11 @@
+# eCommerce Apparels Store Website
+This website features beautiful UI and it's responsive on all screen types. This is only front-end. The data is generated with the help of js.
+
+### Tech Stack -
+- HTML
+- CSS
+- JavaScript
+
+### Project Screenshots -
+
+
---
+++ Web Development/Advanced/E-Comm Apparels Store Website/js/slickslider.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+});
