100c90d33410329cd965345bc423968ec6ddfc6b
Nidhish Chauhan <84083897+Nidhish-07@users.noreply.github.com>
2023-03-15 15:01:56 +0530

    Update main.js

--- Web Development/Intermediate/Arch Bot/client/main.js
+++ Web Development/Intermediate/Arch Bot/client/main.js
@@ -71,13 +71,15 @@ const submitHandler = async (event) => {
 
   loader(messageM)
 
-  // const response = await fetch("http://localhost:8080", {
-  const response = await fetch("http://localhost:8080", {
-    method: 'POST', headers: {
-      'Content-type': 'application/json',
+
+  const response = await fetch("https://arch-bot.onrender.com/", {
+    // const response = await fetch("http://localhost:8080", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
     },
-    body: JSON.stringify({ prompt: data.get('prompt') })
-  })
+    body: JSON.stringify({ prompt: data.get("prompt") }),
+  });
 
 
   clearInterval(timeInterval)
